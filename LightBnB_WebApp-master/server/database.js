72e36181d57e99c760326745bb8f3901615fd9a3
Asha Alibarre <aalibarre@gmail.com>
2021-12-06 00:29:01 +0000

    added the add property function also correctly formated the return result.rows

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,7 +17,9 @@ const pool = new Pool({
 const getUserWithEmail = function(email) {
  return pool
   .query(`SELECT * FROM users WHERE email = $1`, [email])
-  .then((result) => result.rows)
+  .then((result) => {
+    return result.rows[0];
+  })
   .catch((err) => {
     console.log(err.message);
   });
@@ -32,7 +34,9 @@ exports.getUserWithEmail = getUserWithEmail;
 const getUserWithId = function(id) {
   return pool
   .query(`SELECT * FROM users WHERE id = $1`, [id])
-  .then((result) => result.rows)
+  .then((result) => {
+    return result.rows[0];
+  })
   .catch((err) => {
     console.log(err.message);
   });
@@ -49,7 +53,9 @@ const addUser =  function(user) {
   return pool
   .query(`INSERT INTO users (name, email, password) 
   VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password] )
-  .then((result) => result.rows)
+  .then((result) => {
+    return result.rows;
+  })
   .catch((err) => {
     console.log(err.message);
   });
@@ -70,7 +76,9 @@ const getAllReservations = function(guest_id, limit = 10) {
   JOIN properties ON properties.id = reservations.property_id
   WHERE guest_id = $1
   LIMIT $2; `,[guest_id, limit])
-  .then((result) => result.rows)
+  .then((result) => {
+    return result.rows;
+  })
   .catch((err) => {
     console.log(err.message);
   });
@@ -98,7 +106,7 @@ exports.getAllReservations = getAllReservations;
   }
 
   if (options.minimum_price_per_night) {
-    console.log('what is this',options.minimum_price_per_night)
+    //console.log('minmum price per night',options.minimum_price_per_night)
     queryParams.push(options.minimum_price_per_night);
     if (queryString.includes('WHERE')) {
       queryString += `AND cost_per_night > $${queryParams.length} `;
@@ -139,10 +147,18 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+  .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING*; `, 
+  [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, 
+  property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, 
+  property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 
 
